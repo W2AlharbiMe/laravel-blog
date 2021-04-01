d662f8b7656aeca4c3ec61d1ef98ccb48ded316a
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-04-01 22:54:10 +0300

    [style](component): extract nav-link tailwind classes

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,8 @@
-@props(['active'])
+@props(['active' => false])
 
 @php
-$classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-indigo-400 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+$classes = 'nav-link ';
+$classes .= $active ? 'nav-link-active' : 'nav-link-default';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
