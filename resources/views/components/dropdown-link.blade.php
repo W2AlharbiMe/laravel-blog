af39850775008be4930fd9830fa207420cf37d74
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-04-02 22:56:26 +0300

    style(comp): improve dropdown link

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,9 @@
-<a {{ $attributes->merge(['class' => 'block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+@props(['bghover' => 'hover:bg-indigo-100'])
+
+@php
+    $classes = $bghover;
+    $classes .= ' block px-4 py-2 text-sm leading-5 text-gray-700  focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out';
+@endphp
+
+<a
+    {{ $attributes->merge(['class' => $classes]) }}>{{ $slot }}</a>
